2d8faf79ef118fd1eba18ccba5df9d3c4aa988cc
Oskar <oskarradonkimball@gmail.com>
2016-02-03 15:59:53 -0800

    Add links to scripts and jQuery

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+$(function(){
+  $('img.post').each(function(){
+    $(this).css({'margin' : getRandomInt(-50,50)});
+  });
+});
