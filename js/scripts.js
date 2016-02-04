4ced8b8e1056175d978712ac5f56b8dcd34b45e2
Oskar <oskarradonkimball@gmail.com>
2016-02-04 15:52:14 -0800

    Make draggable images

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+//
+// $(function(){
+//   $('img.post').each(function(){
+//     $(this).css({'margin' : getRandomInt(-50,50)});
+//   });
+// });
 
-$(function(){
-  $('img.post').each(function(){
-    $(this).css({'margin' : getRandomInt(-50,50)});
-  });
+$(function() {
+  $(".post").draggable();
 });
