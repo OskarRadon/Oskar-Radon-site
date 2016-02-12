12a1a80d116535c105d347878cf788048eff638d
Oskar <oskarradonkimball@gmail.com>
2016-02-12 15:09:58 -0800

    Re-format all pages to use container div

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-// function getRandomInt(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// }
-//
-// $(function(){
-//   $('img.post').each(function(){
-//     $(this).css({'margin' : getRandomInt(-50,50)});
-//   });
-// });
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+$(function(){
+  $('img.post').each(function(){
+    $(this).css({'margin' : getRandomInt(-150,0)});
+  });
+});
 
 $(function() {
   $(".post").draggable({stack: ".post"});
