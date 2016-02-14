f3ce3695a103db16ab7dca57d4cef9a59429e610
Oskar <oskarradonkimball@gmail.com>
2016-02-14 14:39:54 -0800

    No JS for right now
    🍕

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+// var mq = window.matchMedia('@media all and (min-width: 700px)');
+// if(mq.matches) {
+//     // the width of browser is more then 700px
+// } else {
+//     // the width of browser is less then 700px
+// }
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
